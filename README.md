33674ee92980c76f537369453f1efd8a146f931b
marmei21 <79398228+marmei21@users.noreply.github.com>
2023-06-13 15:42:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vuejs.de - Composition API - Task: Create your first composable
+
+Build a composition function which implements the following logic:
+
+1. An intial value can be applied (true/false)
+2. A function which toggles that value
+3. A function which can be used to set the value to a specific state
+
+Use that function for all 3 use cases inside MyExample.vue.
+Use an extra file for that function and import it inside the component.
---
+++ src/components/MyExample.vue
@@ -0,0 +1,58 @@
+<template>
+  <h1>Lorem ipsum</h1>
+  <div>...</div>
+  <hr />
+  <div>
+    <button @click="togglePrivileges">Toggle privileges</button>
+    <button @click="toggleSessions">Toggle sessions</button>
+    <button @click="togglePurchases">Toggle purchases</button>
+  </div>
+  <hr />
+  <div v-if="showPrivileges">
+    <h2>User privileges</h2>
+    <div>List of settings...</div>
+  </div>
+  <div v-if="showSessions">
+    <h2>Active Sessions</h2>
+    <div>List of active sessions...</div>
+  </div>
+  <div v-if="showPurchases">
+    <h2>Purchases</h2>
+    <div>List of purchases...</div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const showPrivileges = false;
+    const showSessions = true;
+    const showPurchases = true;
+
+    function togglePrivileges() {
+      console.log('toggle privileges');
+    }
+
+    function toggleSessions() {
+      console.log('toggle sessions');
+    }
+
+    function togglePurchases() {
+      console.log('toggle purchases');
+    }
+
+    return {
+      showPrivileges,
+      togglePrivileges,
+      showSessions,
+      toggleSessions,
+      showPurchases,
+      togglePurchases,
+    };
+  },
+});
+</script>
+
+<style></style>
